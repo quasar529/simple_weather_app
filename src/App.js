3515134f08220f4f994449be79eff1178f34acfa
quasar529 <46216280+quasar529@users.noreply.github.com>
2020-05-21 00:10:33 +0900

    add Busan, deploy page

--- src/App.js
+++ src/App.js
@@ -4,12 +4,14 @@ import Home from "./routes/Home";
 import Seoul from "./components/Seoul";
 import Jeju from "./components/Jeju";
 import Gwangju from './components/Gwangju';
+import Busan from './components/Busan';
 function App(){
   return (<HashRouter>
     <Route path='/' exact={true} component={Home} />
     <Route path='/seoul' component={Seoul} />
     <Route path='/jeju' component={Jeju} />
     <Route path='/gwangju' component={Gwangju} />
+    <Route path='/busan' component={Busan} />
   </HashRouter>
   );
 }
--- src/routes/Home.js
+++ src/routes/Home.js
@@ -7,6 +7,7 @@ function Cities(){
             <Link to='/seoul' className='seoul'>Seoul</Link>
             <Link to='/jeju' className='jeju'>Jeju</Link>
             <Link to='/gwangju' className='gwangju'>Gwangju</Link>
+            <Link to='/busan' className='busan'>Busan</Link>
         </div>
     )
 }
